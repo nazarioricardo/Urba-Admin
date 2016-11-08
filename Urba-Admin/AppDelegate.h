774339e0954ba944f42267ee9be25f2897ac3aa1
Ricardo Nazario <nazarioricardo@gmail.com>
2016-11-08 14:14:31 -0400

    Initial Commit

---
+++ Urba-Admin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Urba-Admin
+//
+//  Created by Ricardo Nazario on 11/8/16.
+//  Copyright © 2016 Ricardo Nazario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Urba-Admin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Urba-Admin
+//
+//  Created by Ricardo Nazario on 11/8/16.
+//  Copyright © 2016 Ricardo Nazario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
